f25b225e964a0a0487586374e56c86669e8fb044
Ilan <smilan273@gmail.com>
2023-05-10 18:53:08 +0300

    first upload

---
+++ receiver.py
@@ -0,0 +1,86 @@
+import socket
+import time
+from statistics import mean
+
+
+ID1 = 6396
+ID2 = 58
+
+file_path = "receiver_file.txt"
+file = open(file_path , 'wb')
+
+part1_time = []
+part2_time = []
+
+receiver_ip = '127.0.0.1'
+receiver_port = 9999
+
+receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+buffer_size = receiver_socket.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)
+
+print("Creating a connection...")
+receiver_socket.bind((receiver_ip,receiver_port))
+
+receiver_socket.listen(1)
+print("listening on IP:",receiver_ip," Port:",receiver_port)
+
+sender_socket, sender_address = receiver_socket.accept()
+print("connection Succeeded!")
+
+file_len = int(sender_socket.recv(buffer_size))
+
+while file_len != -1:
+    print("Defines the CC algorithm be reno.")
+    receiver_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b'reno')
+
+    start_time = time.time()
+
+    data_size1 = 0
+    print("receives the first part of the file")
+    while data_size1 < file_len // 2:
+        chunk = sender_socket.recv(buffer_size)
+        data_size1 += len(chunk)
+        file.write(chunk)
+
+    first_part_time = time.time() - start_time
+    part1_time.append(first_part_time)
+
+    print("Sending OK to the sender")
+    sender_socket.send((str(ID1 ^ ID2)).encode())
+
+    print("Defines the CC algorithm be Cubic..")
+    receiver_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b'cubic')
+
+    start_time = time.time()
+    print("receives the second part of the file")
+    data_size2 = 0
+    while data_size2 < file_len - data_size1:
+        chunk = sender_socket.recv(buffer_size)
+        data_size2 += len(chunk)
+        file.write(chunk)
+
+    second_part_time = time.time() - start_time
+    part2_time.append(second_part_time)
+
+    print("Sending OK to the sender")
+    sender_socket.send((str(ID1 ^ ID2)).encode())
+
+    decision = sender_socket.recv(buffer_size).decode()
+    print(decision)
+    if decision == "Stop sending the file":
+        file_len = -1
+        sender_socket.send("Thank you, goodbye!".encode())
+
+for i in range(len(part1_time)):
+    print("Time of receiving number " , i , ": ")
+    print("part 1: " , part1_time[i] , " seconds")
+    print("part 2: " , part2_time[i] , " seconds")
+
+print("Average of part 1:", mean(part1_time), "seconds")
+print("Average of part 2:", mean(part2_time), "seconds")
+print("Total Average: ", mean(part1_time+part2_time), "seconds")
+
+
+print("Close the connection..")
+receiver_socket.close()
+file.close()
---
+++ sender.py
@@ -0,0 +1,78 @@
+import socket
+import os
+
+ID1 = 6396
+ID2 = 58
+
+
+def check_got(xor):
+    bool = xor == (ID1 ^ ID2)
+    return bool
+
+file_path = "myFile1.txt"
+file = open(file_path, "r" )
+
+file_len = os.path.getsize(file_path)
+half_len = file_len // 2
+
+receiver_ip = '127.0.0.1'
+receiver_port = 9999
+
+sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+buffer_size = sender_socket.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)
+
+print("Connecting to receiver...")
+sender_socket.connect((receiver_ip, receiver_port))
+
+print("connection Succeeded!")
+
+# read and split the file
+file_read = file.read()
+
+part1 = file_read[:half_len]
+part2 = file_read[half_len:]
+
+file.close()
+print("Sending the len of the file")
+sender_socket.send(str(file_len).encode())
+
+send = True
+while send is True:
+    print("Sending the len of the file")
+    sender_socket.send(str(file_len).encode())
+
+    print("Defines the CC algorithm be Reno.")
+    sender_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b'reno')
+
+
+    print("Sending the first part of the file..")
+    sender_socket.send(part1.encode())
+
+    xor = float(sender_socket.recv(buffer_size).decode())
+    if check_got(xor) is False:
+        Exception
+    else:
+        print("Succeeded!")
+
+    print("Defines the CC algorithm be Cubic")
+    sender_socket.setsockopt(socket.IPPROTO_TCP , socket.TCP_CONGESTION , b'cubic')
+
+    print("Sending the second part of thr file..")
+    sender_socket.send(part2.encode())
+
+    xor = float(sender_socket.recv(buffer_size).decode())
+    if check_got(xor) is False:
+        raise Exception
+    else:
+        print("Succeeded!")
+
+    again = input("Should you send the file again? (y/n):")
+    if again == "n":
+        send = False
+        sender_socket.send("Stop sending the file".encode())
+        sender_socket.recv(buffer_size).decode()
+    else:
+        sender_socket.send("Sending the file again!".encode())
+
+ sender_socket.close()
+
